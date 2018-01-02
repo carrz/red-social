06821b9fe9cf450e493bd8268ecb91f5c80cf813
carrz <carla.zumaran@gmail.com>
2018-01-02 18:31:52 -0300

    Rollers maqueteado al 50%

---
+++ red-social/assets/js/app.js
@@ -0,0 +1,12 @@
+
+//Firebase
+
+var config = {
+    apiKey: "AIzaSyAVbjIYJMkmhNLHinmsiMR5dbbVBScUTqQ",
+    authDomain: "red-social-spring3.firebaseapp.com",
+    databaseURL: "https://red-social-spring3.firebaseio.com",
+    projectId: "red-social-spring3",
+    storageBucket: "",
+    messagingSenderId: "949348878375"
+  };
+  firebase.initializeApp(config);
