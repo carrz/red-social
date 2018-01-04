6eeda1edb1a154630e4e9f4c26b4d07f20fbaa94
carrz <carla.zumaran@gmail.com>
2018-01-04 14:54:48 -0300

    Funcionalidad del like, lista

--- red-social/assets/js/app.js
+++ red-social/assets/js/app.js
@@ -42,11 +42,21 @@ $(document).ready(function(){
 
 
 //Cambiando icono <3 (No funciona)
-$('#heart11').click(function(){
-  
+$('#heart1').click(function(){
 	$('#heart1').remove();
   $('#a-heart1').append("<i class='material-icons black-text'>favorite</i>");
 });
+
+$('#heart2').click(function(){
+  $('#heart2').remove();
+  $('#a-heart2').append("<i class='material-icons black-text'>favorite</i>");
+});
+
+$('#heart3').click(function(){
+  $('#heart3').remove();
+  $('#a-heart3').append("<i class='material-icons black-text'>favorite</i>");
+});
+
 
 //Cargando nuevo post con foto del dispositivo
 $('#postDevice').click(function(){
@@ -54,7 +64,7 @@ $('#postDevice').click(function(){
   var photo = $('#blah').attr('src');
   var textPost = $('#icon_prefix2').val();
 
-  $('#new-post').append("<div class='card'>" +
+  $('#new-post').prepend("<div class='card'>" +
       "<div class='card-image'>" +
         "<img src='" + photo + "' alt='...'>" +
         "<span class='card-title'>Name</span>" +
@@ -75,7 +85,7 @@ $('#postUrl').click(function(){
   var photo = $('#webUrl').val();
   var textPost = $('#icon_prefix1').val();
 
-  $('#new-post').append("<div class='card'>" +
+  $('#new-post').prepend("<div class='card'>" +
       "<div class='card-image'>" +
         "<img src='" + photo + "' alt='...'>" +
         "<span class='card-title'>Name</span>" +
@@ -95,7 +105,7 @@ $('#postText').click(function(){
 
   var textPost = $('#textToPost').val();
 
-  $('#new-post').append("<div class='card black'>" +
+  $('#new-post').prepend("<div class='card black'>" +
       "<a class='btn-floating halfway-fab waves-effect waves-light yellow lighten-1'>" +
           "<i class='material-icons black-text'>favorite_border</i>" +
         "</a>" +
